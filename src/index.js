a243d31b5c4f35306f2efea9a67eaa7b513fede1
Yusuf Abdul <yusufabdul212112@gmail.com>
2024-11-02 03:02:43 +0300

    adds new file

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Article from "../components/Article";
+
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
